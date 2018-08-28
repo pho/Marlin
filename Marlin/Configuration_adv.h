7614666acd5661a31f2fa40bb26c7f0c2a18b6c0
pho <phofin@gmail.com>
2018-08-28 18:50:07 +0200

    Enable SCROLL_LONG_FILENAMES

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -592,8 +592,8 @@
    */
   //#define POWER_LOSS_RECOVERY
   #if ENABLED(POWER_LOSS_RECOVERY)
-    //#define POWER_LOSS_PIN   44     // Pin to detect power loss
-    //#define POWER_LOSS_STATE HIGH   // State of pin indicating power loss
+  //#define POWER_LOSS_PIN   44     // Pin to detect power loss
+  //#define POWER_LOSS_STATE HIGH   // State of pin indicating power loss
   #endif
 
   /**
@@ -638,7 +638,7 @@
   //#define LONG_FILENAME_HOST_SUPPORT
 
   // Enable this option to scroll long filenames in the SD card menu
-  //#define SCROLL_LONG_FILENAMES
+  #define SCROLL_LONG_FILENAMES
 
   /**
    * This option allows you to abort SD printing when any endstop is triggered.
